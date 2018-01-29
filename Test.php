84f702e423a5b8d9508c95fb7a2a16fae23074c5
simon.zhang <simon.zhang@kilimall.com>
2018-01-29 19:41:16 +0800

    git 测试

--- Test.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace nebtden;
-class Test{
-  public function __construct(){
-    echo 111;
-  }
-}
---
+++ src/simon.php
@@ -0,0 +1,14 @@
+<?php
+namespace nebtdenclass;
+/**
+ * Created by PhpStorm.
+ * User: Marser
+ * Date: 2018/1/29
+ * Time: 19:22
+ */
+class simon{
+    public function __construct()
+    {
+        echo 'nebtdens,simon class';
+    }
+}
